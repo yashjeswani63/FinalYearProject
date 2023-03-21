2d2b880b8e0ebd03662f768720940c5dbaf8e52b
Yash Jeswani <yashjeswani63@gmail.com>
2023-03-21 22:35:56 +0530

    first commit

---
+++ client/src/pages/Object.js
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Object() {
+  return (
+       <Spline scene="https://prod.spline.design/o8tef3o9uAMWmwAN/scene.splinecode" />
+  );
+}
+
+
+
